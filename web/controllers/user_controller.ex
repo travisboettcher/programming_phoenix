c5e9b62e54126c879db0f5c59a680d8322d0511c
Travis Boettcher <tboettcher@gmail.com>
2016-07-02 07:33:42 -0500

    Add validation errors for users.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,6 +7,18 @@ defmodule Rumbl.UserController do
     render conn, "new.html", changeset: changeset
   end
 
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
